09ec6a9d2b375cf7a3c4a9805722a7ca7aa49806
Basara.Wu(吳欣昌) <Basara.Wu@TCI-BIO.com>
2017-03-22 14:06:35 +0800

    Add  Simple HLS Stream Server Source

---
+++ index.js
@@ -0,0 +1,58 @@
+const koa = require("koa");
+const mount = require("koa-mount");
+const koastatic = require("koa-static");
+
+//const router =  new require('koa-router')();
+
+const logger = require("koa-logger");
+
+const app = new koa();
+const port = process.env.PORT || "3000";
+const http = require('http');
+const ffmpegPath = "D:\\Tools\\ffmpeg-20170321-db7a05d-win64-static\\bin\\ffmpeg.exe";
+const ffprobePath = "D:\\Tools\\ffmpeg-20170321-db7a05d-win64-static\\bin\\ffprobe.exe";
+const sourcePath = "rtsp://xxx.xxxx.xxx.xxx/xx"
+
+
+let io;
+console.log("here");
+
+console.log(__dirname + '/public');
+app.use(logger());
+app.use(mount('/public',koastatic(__dirname + '/public')));
+console.log(__dirname + '/stream');
+app.use(mount("/stream",koastatic(__dirname + '/stream')));
+app.use(mount("/scripts",koastatic(__dirname + '/scripts')));
+
+// app.use(ctx=>{
+//     console.log("2");
+//     ctx.body="Hello world";
+// });
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx);
+});
+
+let server = app.listen(port, () => {
+  console.log("started http://localhost:", port);
+  recordTest();
+});
+
+function recordTest(){
+    var ffmpeg = require('fluent-ffmpeg');
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
+ffmpeg({ source: sourcePath })
+  .addOption('-g',10)
+  .addOption('-hls_list_size', 2)
+  .addOption('-hls_time', 3)
+  .addOption('-hls_flags', 'delete_segments')
+  .on('end', function () {
+    console.log('ended recording');
+    process.exit(0);
+  })
+  .on('error', function (err) {
+    console.log('an error happened: ' + err.message);
+  })
+  .save( __dirname + '/stream/test.m3u8');
+}
